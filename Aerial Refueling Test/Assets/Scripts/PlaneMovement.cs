37f6038f2c169acfe8bf37227e08e8cde39adcfa
zuderg@gmail.com <>
2023-02-01 11:45:12 -0600

    documentation

--- Aerial Refueling Test/Assets/Scripts/PlaneMovement.cs
+++ Aerial Refueling Test/Assets/Scripts/PlaneMovement.cs
@@ -28,9 +28,10 @@ public class PlaneMovement : MonoBehaviour
     public float AngOfAttack;
 
     /// <summary>
-    /// how much throttle
+    /// the current throttle level
     /// </summary>
     [Range(-1, 1)]
+    [HideInInspector]
     public float throttle;
 
     /// <summary>
@@ -57,6 +58,10 @@ public class PlaneMovement : MonoBehaviour
     [Range(0f, 2000f)]
     public float lift;
 
+    /// <summary>
+    /// pointer to the rigidbody this script is affecting
+    /// </summary>
+    [HideInInspector]
     public Rigidbody plane;
 
     /// <summary>
@@ -72,98 +77,239 @@ public class PlaneMovement : MonoBehaviour
     public float LRSpeed;
 
     /// <summary>
-    /// how much flaps rotate in flight
+    /// key to pitch the plane forward
     /// </summary>
-    public float flapAmount;
-
-    /// <summary>
-    /// list of all left flaps
-    /// </summary>
-    public List<GameObject> flapsLeft;
-
-    /// <summary>
-    /// list of all right flaps
-    /// </summary>
-    public List<GameObject> flapsRight;
-
-    // move params
+    [HideInInspector]
     public KeyCode forward;
+    /// <summary>
+    /// key to pitch the plane back
+    /// </summary>
+    [HideInInspector]
     public KeyCode backward;
+    /// <summary>
+    /// key to roll the plane left
+    /// </summary>
+    [HideInInspector]
     public KeyCode left;
+    /// <summary>
+    /// key to roll the plane right
+    /// </summary>
+    [HideInInspector]
     public KeyCode right;
+    /// <summary>
+    /// key to yaw the plane left
+    /// </summary>
+    [HideInInspector]
     public KeyCode yawL;
+    /// <summary>
+    /// key to yaw the plane right
+    /// </summary>
+    [HideInInspector]
     public KeyCode yawR;
+    /// <summary>
+    /// key to increase the throttle
+    /// </summary>
+    [HideInInspector]
     public KeyCode throttleUp;
+    /// <summary>
+    /// key to increase the throttle
+    /// </summary>
+    [HideInInspector]
     public KeyCode throttleDown;
-
-    // Grab GameObject to calculate distance between plane and refueller
+    /// <summary>
+    /// pointer to the C5 gameObject
+    /// </summary>
+    [HideInInspector]
     public GameObject C5go;
+    /// <summary>
+    /// pointer to the KC135 gameObject
+    /// </summary>
+    [HideInInspector]
     public GameObject KC135go;
+    /// <summary>
+    /// position vector of the C5
+    /// </summary>
     Vector3 C5;
+    /// <summary>
+    /// position vector of the KC135
+    /// </summary>
     Vector3 KC135;
 
-    [Range(0f, 100f)]
-    public float minMaxDistanceX = 60;
-    [Range(0f, 100f)]
-    public float minMaxDistanceY = 60;
-    [Range(0f, 150f)]
-    public float minMaxDistanceZ = 120;
-
-    [Range(0f, 100f)]
-    public float minMaxRotationX = 60;
-    [Range(0f, 100f)]
-    public float minMaxRotationY = 60;
-    [Range(0f, 150f)]
-    public float minMaxRotationZ = 120;
-
+    /// <summary>
+    /// if true, auto-target C5 movement
+    /// </summary>
+    [HideInInspector]
     public bool targetMode;
-
+    /// <summary>
+    /// if true, control C5 with keyboard
+    /// </summary>
+    [HideInInspector]
     public bool keyboardMode;
+    /// <summary>
+    /// if true, control C5 with flight stick
+    /// </summary>
+    [HideInInspector]
     public bool joystickMode;
+    /// <summary>
+    /// if true, control C5 with PS4 controller
+    /// </summary>
+    [HideInInspector]
     public bool controllerMode;
+    /// <summary>
+    /// if C5 is player-controlled
+    /// </summary>
+    [HideInInspector]
     public bool playerC5;
 
+    /// <summary>
+    /// playerInput object for C5 control
+    /// </summary>
     private PlayerInput playerInput;
 
+    /// <summary>
+    /// X/Y movement from joystick
+    /// </summary>
+    [HideInInspector]
     private InputAction moveAction;
+    /// <summary>
+    /// throttle action from joystick
+    /// </summary>
     private InputAction thrustAction;
+    /// <summary>
+    /// yaw action from joystick
+    /// </summary>
     private InputAction yawAction;
 
+    /// <summary>
+    /// X/Y movement from PS4 controller
+    /// </summary>
     private InputAction moveActionController;
+    /// <summary>
+    /// X/Y movement from PS4 controller
+    /// </summary>
     private InputAction thrustActionController;
+    /// <summary>
+    /// X/Y movement from PS4 controller
+    /// </summary>
     private InputAction yawActionController;
+    /// <summary>
+    /// target position of C5 for auto-pilot
+    /// </summary>
+    [HideInInspector]
     public Vector3 targetPos;
+    /// <summary>
+    /// maximum roll amount for C5
+    /// </summary>
     public float rollMax;
+    /// <summary>
+    /// maximum pitch amount for C5
+    /// </summary>
     public float pitchMax;
 
-    Vector3 startPos;
-    Quaternion startRot;
+    /// <summary>
+    /// C5 position at start of sim
+    /// </summary>
+    private Vector3 startPos;
+    /// <summary>
+    /// C5 rotation at start of sim
+    /// </summary>
+    private Quaternion startRot;
 
+    /// <summary>
+    /// toggle for volumetric clouds
+    /// </summary>
     private Boolean volumetricClouds;
+    /// <summary>
+    /// toggle for fog effects
+    /// </summary>
     private Boolean fogToggle;
+    /// <summary>
+    /// time of day setting
+    /// </summary>
     private int timeOfDay;
 
+    /// <summary>
+    /// pointer to volumetric clouds gameobject
+    /// </summary>
+    [HideInInspector]
     public GameObject volumetricCloudsObject;
+    /// <summary>
+    /// pointer to fog controller gameObject
+    /// </summary>
+    [HideInInspector]
     public GameObject fogObject;
 
+    /// <summary>
+    /// volumetric clouds volume object
+    /// </summary>
+    [HideInInspector]
     public Volume volume;
+    /// <summary>
+    /// used in cloud settings
+    /// </summary>
     VolumetricClouds cloud;
+    /// <summary>
+    /// used in cloud settings
+    /// </summary>
     VolumetricClouds clouds;
+    /// <summary>
+    /// used in fog settings
+    /// </summary>
     Fog fogVolume;
+    /// <summary>
+    /// used in fog settings
+    /// </summary>
     Fog fog;
 
+    /// <summary>
+    /// game object pointer to sunlight 
+    /// </summary>
+    [HideInInspector]
     public GameObject sun;
 
+    /// <summary>
+    /// pointer to EndScreen script
+    /// </summary>
+    [HideInInspector]
     public EndScreen EndScreen;
+    /// <summary>
+    /// pointer to BoomArmMovement Script
+    /// </summary>
+    [HideInInspector]
     public BoomArmMovement BoomArmMovement;
+    /// <summary>
+    /// how much fuel has been delivered to the c5
+    /// </summary>
+    [HideInInspector]
     public float fuel;
+    /// <summary>
+    /// used to track collisions with the boom arm
+    /// </summary>
+    [HideInInspector]
     public int C5Counter;
 
+    /// <summary>
+    /// tracks X distance between aircraft
+    /// </summary>
+    [HideInInspector]
     public float distanceX;
+    /// <summary>
+    /// tracks Y distance between aircraft
+    /// </summary>
+    [HideInInspector]
     public float distanceY;
+    /// <summary>
+    /// tracks Z distance between aircraft
+    /// </summary>
+    [HideInInspector]
     public float distanceZ;
 
-    bool intToBool(int val)
+    /// <summary>
+    /// converts an int into a bool (used in player preference handling)
+    /// </summary>
+    /// <param name="val">value to convert</param>
+    /// <returns></returns>
+    bool IntToBool(int val)
     {
         if (val != 0)
             return true;
@@ -171,14 +317,15 @@ public class PlaneMovement : MonoBehaviour
             return false;
     }
 
+    // called upon script startup
     public void Start()
     {
+        // setup pointers
         plane = GetComponent<Rigidbody>();
         startPos = transform.localPosition;
-        // Debug.Log(startPos);
         startRot = transform.rotation;
-
         playerInput = GetComponent<PlayerInput>();
+
         moveAction = playerInput.actions["Move2"];
         thrustAction = playerInput.actions["Thrust2"];
         yawAction = playerInput.actions["Yaw2"];
@@ -187,15 +334,14 @@ public class PlaneMovement : MonoBehaviour
         thrustActionController = playerInput.actions["Thrust1"];
         yawActionController = playerInput.actions["Yaw1"];
 
+        keyboardMode = IntToBool(PlayerPrefs.GetInt("inputKeyboardC5"));
+        joystickMode = IntToBool(PlayerPrefs.GetInt("inputJoystickC5"));
+        controllerMode = IntToBool(PlayerPrefs.GetInt("inputControllerC5"));
 
-        keyboardMode = intToBool(PlayerPrefs.GetInt("inputKeyboardC5"));
-        joystickMode = intToBool(PlayerPrefs.GetInt("inputJoystickC5"));
-        controllerMode = intToBool(PlayerPrefs.GetInt("inputControllerC5"));
+        playerC5 = IntToBool(PlayerPrefs.GetInt("playerC5"));
 
-        playerC5 = intToBool(PlayerPrefs.GetInt("playerC5"));
-
-        volumetricClouds = intToBool(PlayerPrefs.GetInt("volumetricCloudsBool"));
-        fogToggle = intToBool(PlayerPrefs.GetInt("fogBool"));
+        volumetricClouds = IntToBool(PlayerPrefs.GetInt("volumetricCloudsBool"));
+        fogToggle = IntToBool(PlayerPrefs.GetInt("fogBool"));
         timeOfDay = PlayerPrefs.GetInt("TimeOfDay");
 
         volume = volumetricCloudsObject.GetComponent<Volume>();
@@ -225,15 +371,20 @@ public class PlaneMovement : MonoBehaviour
 
     void Update()
     {
+        // check if plane needs to be reset each frame
         ResetPlane();
 
         // creating blank torque force
         Vector3 torq = new(0, 0, 0);
+
+        // autopilot
         if (targetMode)
         {
-            // Debug.Log(transform.rotation.eulerAngles);
+            // calculate change in position and rotation needed
             Vector3 deltaPos = targetPos - transform.localPosition;
             Vector3 adjustedRot = transform.localEulerAngles;
+
+            // convert from 0->360 to -180->180
             if (adjustedRot.x > 180)
                 adjustedRot.x -= 360;
             if (adjustedRot.y > 180)
@@ -243,6 +394,8 @@ public class PlaneMovement : MonoBehaviour
 
             // autoflight for roll
             float xdiff = Vector3.Dot(deltaPos, new Vector3(transform.right.x, 0, transform.right.z));
+
+            // make bigger adjustments if roll is very difference
             if (Mathf.Abs(xdiff) > 2)
             {
                 // left
@@ -252,6 +405,7 @@ public class PlaneMovement : MonoBehaviour
                 else if (xdiff > 0 && adjustedRot.z > -rollMax)
                     torq -= 0.01f * rollSpeed * Time.deltaTime * transform.forward;
             }
+            // make smaller adjustments with smaller difference
             else
             {
                 transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
@@ -274,24 +428,30 @@ public class PlaneMovement : MonoBehaviour
             }
 
             // autoflight for throttle
+            // calculate z diff between two aircraft
             float zdiff = Vector3.Dot(deltaPos, new Vector3(transform.forward.x, 0, transform.forward.z));
+            // make large adjustments at large difference
             if (Mathf.Abs(zdiff) > 5)
             {
+                // increase throttle
                 if (zdiff > 0)
                     throttle = Mathf.Clamp(throttle + (throttleChange * Time.deltaTime), -1, 1);
 
+                // decrease throttle
                 else if (zdiff < 0)
                     throttle = Mathf.Clamp(throttle - (throttleChange * Time.deltaTime), -1, 1);
             }
+            // make smaller adjustments at small difference
             else
             {
                 throttle = 0;
             }
-            // Debug.Log(zdiff);
         }
 
+        // player-controlled
         else
         {
+            // controll with keyboard
             if (keyboardMode)
             {
                 // adding roll to torque vector
@@ -395,108 +555,95 @@ public class PlaneMovement : MonoBehaviour
         plane.angularVelocity = torq;
     }
 
-    // fun script to make flaps move on C5
-    void AdjustFlaps(string s)
-    {
-        if (s == "left")
-        {
-            foreach (GameObject flap in flapsLeft)
-                flap.transform.eulerAngles = -Vector3.right * flapAmount;
-            foreach (GameObject flap in flapsRight)
-                flap.transform.eulerAngles = Vector3.right * flapAmount;
-        }
-        else if (s == "right")
-        {
-            foreach (GameObject flap in flapsLeft)
-                flap.transform.eulerAngles = Vector3.right * flapAmount;
-            foreach (GameObject flap in flapsRight)
-                flap.transform.eulerAngles = -Vector3.right * flapAmount;
-        }
-        else if (s == "up")
-        {
-            foreach (GameObject flap in flapsLeft)
-                flap.transform.eulerAngles = Vector3.right * flapAmount;
-            foreach (GameObject flap in flapsRight)
-                flap.transform.eulerAngles = Vector3.right * flapAmount;
-        }
-        else if (s == "down")
-        {
-            foreach (GameObject flap in flapsLeft)
-                flap.transform.eulerAngles = -Vector3.right * flapAmount;
-            foreach (GameObject flap in flapsRight)
-                flap.transform.eulerAngles = -Vector3.right * flapAmount;
-        }
-        else if (s == "flat")
-        {
-            foreach (GameObject flap in flapsLeft)
-                flap.transform.eulerAngles = Vector3.zero;
-            foreach (GameObject flap in flapsRight)
-                flap.transform.eulerAngles = Vector3.zero;
-        }
-    }
-
+    /// <summary>
+    /// calculate if planes need to be reset due to distance between
+    /// </summary>
     void ResetPlane()
     {
+        // get positions of C5, KC135, and fuel amount
         C5 = C5go.transform.position;
         KC135 = KC135go.transform.position;
         fuel = BoomArmMovement.fuelAmt;
 
+        // calculate XYZ difference btween C5 and KC135
         Vector3 difference = new Vector3(C5.x - KC135.x, C5.y - KC135.y, C5.z - KC135.z);
 
         distanceX = difference.x;
         distanceY = difference.y;
         distanceZ = difference.z;
 
+        // if enough fuel has been delivered
         if (fuel >= 100)
         {
+            // and autoflight and arm is not clamped
             if (!playerC5 && !BoomArmMovement.clamped)
             {
+                // end sim
                 EndScreen.SetUp();
             }
+            // player-controlled or arm is clamped
             else
             {
+                // if distance is too large between aircraft
                 if (distanceX > 60 || distanceX < -60 || distanceY > 60 || distanceY < -60 || distanceZ > 30 || distanceZ < -50)
+                {
+                    // end sim
                     EndScreen.SetUp();
+                }
             }
         }
+        // not enough fuel has been delivered
         else
         {
+            // if distance is too large between aircraft
             if (distanceX > 60 || distanceX < -60 || distanceY > 60 || distanceY < -60 || distanceZ > 60 || distanceZ < -100)
+            {
+                // end sim
                 EndScreen.SetUp();
+            }
         }
         
     }
 
-    
+    // used to detect colisions between C5 and KC135
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "KC135")
         {
-            Debug.Log(C5Counter);
+            // if too much damage taken
             if (C5Counter >= 100)
+            {
+                // end sim
                 EndScreen.SetUp();
+            }
+            // increase damage
             else
                 C5Counter++;
         }
     }
 
+    /// <summary>
+    /// used to spawn the C5 in-world
+    /// </summary>
+    /// <param name="randPos">if true, C5 has random spawn position, otherwise, just spawn at start position</param>
+    public void SpawnPlane(bool randPos)
+    {
+        transform.localPosition = startPos;
+        transform.rotation = startRot;
 
-       public void SpawnPlane(bool randPos)
+        if (randPos)
         {
-            transform.localPosition = startPos;
-            transform.rotation = startRot;
+        // get random values for position and rotation
+        float rangeRotationX = UnityEngine.Random.Range(-2.5f, 2.5f);
+        float rangeRotationY = UnityEngine.Random.Range(-2.5f, 2.5f);
+        float rangeRotationZ = UnityEngine.Random.Range(-5, 5);
+        float rangeDistanceZ = UnityEngine.Random.Range(-1.25f, 1.25f);
+        float rangeDistanceX = UnityEngine.Random.Range(-0.75f, 0.75f);
+        float rangeDistanceY = UnityEngine.Random.Range(-0.2f, 0.2f);
 
-            if (randPos)
-            {
-                float rangeRotationX = UnityEngine.Random.Range(-2.5f, 2.5f);
-                float rangeRotationY = UnityEngine.Random.Range(-2.5f, 2.5f);
-                float rangeRotationZ = UnityEngine.Random.Range(-5, 5);
-                float rangeDistanceZ = UnityEngine.Random.Range(-1.25f, 1.25f);
-                float rangeDistanceX = UnityEngine.Random.Range(-0.75f, 0.75f);
-                float rangeDistanceY = UnityEngine.Random.Range(-0.2f, 0.2f);
-
-                transform.rotation = Quaternion.Euler(rangeRotationX, rangeRotationY, rangeRotationZ);
-                transform.position += new Vector3(rangeDistanceX, rangeDistanceY, rangeDistanceZ);
-            }
+        // apply random values to position and rotation
+        transform.rotation = Quaternion.Euler(rangeRotationX, rangeRotationY, rangeRotationZ);
+        transform.position += new Vector3(rangeDistanceX, rangeDistanceY, rangeDistanceZ);
         }
+    }
 }
